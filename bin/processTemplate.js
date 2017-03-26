c5748f653c2bc2870d78fa8873cea8728ebb16ef
Jonathan Knapp <jon@coffeeandcode.com>
2017-03-26 01:16:21 -0400

    Add support for CloudFormation short form intrinsic functions used in project

--- bin/processTemplate.js
+++ bin/processTemplate.js
@@ -14,9 +14,36 @@ const ENVType = new yaml.Type('!env', {
   kind: 'scalar'
 });
 
+const GetAttType = new yaml.Type('!GetAtt', {
+  construct: function(data) {
+    return {
+      'Fn::GetAtt': data.split('.')
+    };
+  },
+  kind: 'scalar'
+});
+
+const JoinType = new yaml.Type('!Join', {
+  construct: function(data) {
+    return {
+      'Fn::Join': data
+    };
+  },
+  kind: 'sequence'
+})
+
+const RefType = new yaml.Type('!Ref', {
+  construct: function(data) {
+    return {
+      Ref: data
+    };
+  },
+  kind: 'scalar'
+});
+
 try {
   const template = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../template.yml'), 'utf8'), {
-    schema: yaml.Schema.create([ ENVType ])
+    schema: yaml.Schema.create([ ENVType, GetAttType, JoinType, RefType ])
   });
   fs.writeFileSync(path.join(__dirname, '../template.processed.yml'), yaml.safeDump(template));
 } catch (error) {
